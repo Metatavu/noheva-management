559088baa6d165ee550e4b012b44e033e525d4cd
Ville Juutila <ville.juutila@gmail.com>
2023-08-11 09:33:20 +0300

    Updated build command

--- src/components/floor-plan/floor-plan-crop.tsx
+++ src/components/floor-plan/floor-plan-crop.tsx
@@ -82,7 +82,7 @@ export default class FloorPlanCrop extends React.Component<Props, State> {
    * Event handler for crop component crop end
    */
   private onCropEnd = () => {
-    const cropper = this.cropperRef.current?.cropper;
+    const cropper = this.cropperRef.current;
     if (cropper) {
       const cropperInstance = cropper.cropper;
       if (cropperInstance) {
