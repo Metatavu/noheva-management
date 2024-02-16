c0e0e9fbbb4f81dda5a8c947f7875fffecb3675b
Antti Leppä <antti.leppa@metatavu.fi>
2024-02-16 04:29:57 +0200

    Fine tuned media library loader styles

--- src/styles/components/right-panel-editors/media-library.ts
+++ src/styles/components/right-panel-editors/media-library.ts
@@ -1,15 +1,9 @@
-import theme from "../../theme";
 import createStyles from "@mui/styles/createStyles";
 
 export default createStyles({
 
   loader: {
-    height: "100%",
-    position: "absolute",
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
+    minHeight: 200,
     display: "flex",
     justifyContent: "center",
     alignItems: "center"
